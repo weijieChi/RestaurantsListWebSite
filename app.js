2955151afe9eed2ef7e4db7b07eeedaecc2634b6
weijie <qoo77823@hotmail.com>
2023-10-04 02:08:06 +0800

    install express-handlebars, and finish index page

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 'use strict'
 
 const express = require('express')
-// const { engine } = require('express-handlebars')
+const { engine } = require('express-handlebars')  // use template engine  "express-handlebars"
 const app = express()
 const port = 3000
+
+// express-handlebars
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send(`nodejs it's work!`)
+  res.render(`index`)
 })
 
 app.listen(port, () => {
