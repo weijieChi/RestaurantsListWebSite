c20ab7f8fb5088caf0c5c81496dd3b9329bae41f
weijie <qoo77823@hotmail.com>
2023-10-05 18:54:51 +0800

    finish express-handlebars restaurants page

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const { engine } = require('express-handlebars')  // use template engine  "expre
 const app = express()
 const port = 3000
 
+
 // express-handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.static('public'))
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.get('/', (req, res) => {
-  res.render(`index`)
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+
+  res.render('restaurants', { restaurants })
 })
 
 app.listen(port, () => {
