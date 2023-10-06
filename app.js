d4d2b67cb5fda3d6039b3fd459f005fa58fbe55f
weijie <qoo77823@hotmail.com>
2023-10-06 15:42:20 +0800

    finish restaurant detail page

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.get('/restaurants', (req, res) => {
 
   res.render('restaurants', { restaurants })
 })
+//restaurant
+app.get('/restaurant/:id', (req, res) => {
+  const id = Number(req.params.id)
+  if (id === NaN) res.status(403).render() // 錯誤處理，但是不太清楚要怎麼用
+  // if (!restaurant) res.status(403).render()
+  const restaurant = restaurants.find((item) => item.id === id)
+  res.render('detail', { restaurant })
+})
 
 app.listen(port, () => {
   console.log(`express server on http://localhost:${port}`)
