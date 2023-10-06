f89bfbe18696c34530f8a7a40bc0a066bb8e074d
weijie <qoo77823@hotmail.com>
2023-10-06 20:43:58 +0800

    finish search function

--- app.js
+++ app.js
@@ -19,17 +19,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
+  const keyword = req.query.keyword?.trim()
+  // 搜尋功能
+  const matchedRastaurants = keyword ? restaurants.filter((restaurant) =>
+    Object.values(restaurant).some((property) => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : restaurants
 
-  res.render('restaurants', { restaurants })
+  res.render('restaurants', { restaurants: matchedRastaurants, keyword })
 })
-//restaurant
+
 app.get('/restaurant/:id', (req, res) => {
   const id = Number(req.params.id)
-  if (id === NaN) res.status(403).render() // 錯誤處理，但是不太清楚要怎麼用
-  // if (!restaurant) res.status(403).render()
+  if (id === NaN) res.status(403).render('notFound') // 錯誤處理，但是不太清楚要怎麼用
   const restaurant = restaurants.find((item) => item.id === id)
+  if (!restaurant) res.status(403).render('notFound')
   res.render('detail', { restaurant })
 })
+
 
 app.listen(port, () => {
   console.log(`express server on http://localhost:${port}`)
